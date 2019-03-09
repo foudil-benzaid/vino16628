d875a6ea458340da482ddfaf10c7d3a6ecdc0565
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-03-08 21:37:03 -0500

    Ajouter CDN Bootstrap

--- vues/entete.php
+++ vues/entete.php
@@ -9,7 +9,8 @@
 
 		<meta name="description" content="Un petit verre de vino">
 		<meta name="author" content="Jonathan Martel (jmartel@cmaisonneuve.qc.ca)">
-
+		<!-- CDN Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
